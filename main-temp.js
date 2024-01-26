c2fa3dd9dbf14378d3e2e5dd7efeb12fbdca26f7
ali-parhizain <kamiabparhizain23@gmail.com>
2024-01-26 20:24:19 +0330

    last change

--- main-temp.js
+++ main-temp.js
@@ -1,21 +1,31 @@
 let addTeacherBtn = document.querySelector(".add-teacher"); //use querySelector
 let addStudentBtn = document.querySelector(".add-student"); //use querySelector
 let printBtn  = document.querySelector(".print"); //use querySelector
+var alert = document.querySelector("div.alert");
 var res = document.querySelector("div.res");
+var inp = document.querySelectorAll(".q-input");
+var arr = [];
 
 function emptyInput(i1, i2) {
-    var empty;
-    empty = document.querySelectorAll(".q-input");
-    if ((empty[i1].value.trim().length === 0) || (empty[i2].value.trim().length === 0))  {
-        res.innerHTML = "Inputs must not be empty!";
+    if ((inp[i1].value.trim().length === 0) || (inp[i2].value.trim().length === 0))  {
+        alert.innerHTML = "Inputs must not be empty!";
+        alert.style.color = "red";
+        return(0);
     }
-    return(0);
+    if ((inp[i1].value.trim().length === 0) || (inp[i2].value.trim().length != 0)) {
+        alert.innerHTML = " ";
+    }
+}
+function addinputs(j1,j2) {
+    arr.push(inp[j1].value, inp[j2].value);
+    console.log(arr);
 }
 function addTeacher() {
     var output = emptyInput(0,1);
     if(output == 0){
         return(0);
     }
+    addinputs(0,1);
 }
 
 function addStudent() {
@@ -23,10 +33,15 @@ function addStudent() {
     if(output == 0){
         return(0);
     }
+    addinputs(2,3);
 }
 
 function print() {
-   
+    for (let i = 0; i <= arr.length; i++) {
+        
+        res.innerHTML = `${i+". "+arr+"\n"}`;
+        
+    }
 }
 
 addTeacherBtn.addEventListener("click", addTeacher)
