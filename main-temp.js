82fd02888aac3b11dcc2f34e55439f9d159afe7f
ali-parhizain <kamiabparhizain23@gmail.com>
2024-01-26 14:45:44 +0330

    init

---
+++ main-temp.js
@@ -0,0 +1,19 @@
+let addTeacherBtn //use querySelector
+let addStudentBtn //use querySelector
+let printBtn //use querySelector
+
+function addTeacher() {
+    
+}
+
+function addStudent() {
+
+}
+
+function print() {
+   
+}
+
+addTeacherBtn.addEventListener("click", addTeacher)
+addStudentBtn.addEventListener("click", addStudent)
+printBtn.addEventListener("click", print)
