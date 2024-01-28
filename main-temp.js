b197f49d60731d245416cc0d95305b5d23c4bedd
ali-parhizain <kamiabparhizain23@gmail.com>
2024-01-28 18:40:42 +0330

    add input edited

--- main-temp.js
+++ main-temp.js
@@ -1,31 +1,30 @@
 let addTeacherBtn = document.querySelector(".add-teacher"); //use querySelector
 let addStudentBtn = document.querySelector(".add-student"); //use querySelector
 let printBtn  = document.querySelector(".print"); //use querySelector
-var alert = document.querySelector("div.alert");
+let form = document.querySelectorAll(".keyValue-box")
 var res = document.querySelector("div.res");
 var inp = document.querySelectorAll(".q-input");
-var arr = [];
 
 function emptyInput(i1, i2) {
     if ((inp[i1].value.trim().length === 0) || (inp[i2].value.trim().length === 0))  {
-        alert.innerHTML = "Inputs must not be empty!";
-        alert.style.color = "red";
+        alert("Inputs must not be empty!");
         return(0);
     }
-    if ((inp[i1].value.trim().length === 0) || (inp[i2].value.trim().length != 0)) {
-        alert.innerHTML = " ";
-    }
 }
-function addinputs(j1,j2) {
-    arr.push(inp[j1].value, inp[j2].value);
-    console.log(arr);
+function addinputs(i) {
+    var input1 = document.createElement("input");
+    var input2 = document.createElement("input");
+    input1.className = "q-input";
+    input2.className = "q-input";
+    form[i].appendChild(input1);
+    form[i].appendChild(input2);
 }
 function addTeacher() {
     var output = emptyInput(0,1);
     if(output == 0){
         return(0);
     }
-    addinputs(0,1);
+    addinputs(0);
 }
 
 function addStudent() {
@@ -33,18 +32,11 @@ function addStudent() {
     if(output == 0){
         return(0);
     }
-    addinputs(2,3);
+    addinputs(1);
 }
 
 function print() {
-    for (let i = 0; i < arr.length; i=i+2) {
-
-        var newEl = document.createElement("p");
-        newEl.innerHTML = `${i+1+". "+arr[i]+" "+arr[i+1]}`;
-        res.appendChild(newEl);
-        
-    }
-    // arr.remove(value);
+    
 }
 
 addTeacherBtn.addEventListener("click", addTeacher)
