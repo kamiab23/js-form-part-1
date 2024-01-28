57c7a45fc73803bf1125d687f08f955dca265698
ali-parhizain <kamiabparhizain23@gmail.com>
2024-01-28 20:04:20 +0330

    add and empty completed

--- main-temp.js
+++ main-temp.js
@@ -3,24 +3,39 @@ let addStudentBtn = document.querySelector(".add-student"); //use querySelector
 let printBtn  = document.querySelector(".print"); //use querySelector
 let form = document.querySelectorAll(".keyValue-box")
 var res = document.querySelector("div.res");
-var inp = document.querySelectorAll(".q-input");
 
-function emptyInput(i1, i2) {
-    if ((inp[i1].value.trim().length === 0) || (inp[i2].value.trim().length === 0))  {
-        alert("Inputs must not be empty!");
-        return(0);
+function emptyInput1() {
+    var inp = document.querySelectorAll(".q-input1");
+    // console.log(inp);
+    for (let j = 0; j < inp.length; j++) {
+        console.log(inp[j].value);
+        if (inp[j].value.trim().length === 0)  {
+            alert("Inputs must not be empty!");
+            return(0);
+            
+        }
+    }
+}
+function emptyInput2() {
+    var inp = document.querySelectorAll(".q-input2");
+    // console.log(inp);
+    for (let j = 0; j < inp.length; j++) {
+        if (inp[j].value.trim().length === 0)  {
+            alert("Inputs must not be empty!");
+            return(0);
+        }
     }
 }
 function addinputs(i) {
     var input1 = document.createElement("input");
     var input2 = document.createElement("input");
-    input1.className = "q-input";
-    input2.className = "q-input";
+    input1.className = "q-input1";
+    input2.className = "q-input1";
     form[i].appendChild(input1);
     form[i].appendChild(input2);
 }
 function addTeacher() {
-    var output = emptyInput(0,1);
+    var output = emptyInput1();
     if(output == 0){
         return(0);
     }
@@ -28,7 +43,7 @@ function addTeacher() {
 }
 
 function addStudent() {
-    var output = emptyInput(2,3);
+    var output = emptyInput2();
     if(output == 0){
         return(0);
     }
